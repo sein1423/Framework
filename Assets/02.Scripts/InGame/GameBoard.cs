12a67171d1885acdcc086fbd7ce2295be9b35e68
jannyang <wlghks1725@naver.com>
2023-01-25 18:22:54 +0900

    23_01_25 Update
    Update

--- Assets/02.Scripts/InGame/GameBoard.cs
+++ Assets/02.Scripts/InGame/GameBoard.cs
@@ -37,8 +37,6 @@ public class GameBoard : MonoBehaviour
 
     private void Update()
     {
-        //Debug.Log("Rotation = " + gyroRotationRate);
-        //Debug.Log("Accel = " + gyroUserAccel);
         if (rotating && GameManager.instance.b_gameStart)
         {
             RotateWithDrag();
@@ -66,7 +64,6 @@ public class GameBoard : MonoBehaviour
         gyroRotationRate = Input.gyro.rotationRateUnbiased;
         gyroValue = gyroRotationRate.y * Time.deltaTime * gyroRotateSpeed;
         gyroValue = Mathf.Clamp(gyroValue, -1f, 1f);
-        Debug.Log(gyroValue);
         angle -= gyroValue;
         angle = Mathf.Clamp(angle, -maxDegree, maxDegree);
         transform.eulerAngles = new Vector3(0, 0, angle);
